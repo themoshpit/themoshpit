03cf0760addc2449984a6e184ea8d8740527d0c0
themoshpit <39810724+themoshpit@users.noreply.github.com>
2018-06-09 23:53:33 -0700

    Add files via upload

---
+++ userdata.js
@@ -0,0 +1,28 @@
+let pdata = {
+	/*
+	'user name, no spaces': {
+		firstName: 'John', // First Name
+		lastName: 'Doe', // Last Name
+		realNameVisible: false, // Can People See Your Real Name? true (yes) | false (no)
+		nickName: 'Johnny', // The Name People Will See
+		age: 0, // Your Age
+		ageVisible: true, // Can People See Your Age? true (yes) | false (no)
+		bio: 'This is a bio', // Write About Yourself
+		pictureUrl: 'https://somepics.com/apic.png or ask to upload one', // The Picture That Will Be Displayed
+			// On Your Profile, Put none For No Picture
+		position: 'Rapper / Producer', // What Are You Considered In The Group?
+	},
+	*/
+	'darkheartproductions': {
+		firstName: 'James', // First Name
+		lastName: 'Melix', // Last Name
+		realNameVisible: false, // Can People See Your Real Name? true (yes) | false (no)
+		nickName: 'DarkHeart Productions', // The Name People Will See
+		age: 16, // Your Age
+		ageVisible: true, // Can People See Your Age? true (yes) | false (no)
+		bio: '', // Write About Yourself
+		pictureUrl: 'none', // The Picture That Will Be Displayed
+			// On Your Profile, Put none For No Picture
+		position: 'Producer', // What Are You Considered In The Group?
+	},
+};
